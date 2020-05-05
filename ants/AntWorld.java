4bcfff8300292b55fda088f234f810db71b3c455
Ampersand-Costco <emersoncoskey@gmail.com>
2020-05-04 23:37:59 -0700

    food collection complete

--- ants/AntWorld.java
+++ ants/AntWorld.java
@@ -31,5 +31,20 @@ public class AntWorld extends World
         addObject(food,224,161);
         AntHill antHill = new AntHill();
         addObject(antHill,405,326);
+        Food food2 = new Food();
+        addObject(food2,229,384);
+        Food food3 = new Food();
+        addObject(food3,425,572);
+        Food food4 = new Food();
+        addObject(food4,174,512);
+        Food food5 = new Food();
+        addObject(food5,562,148);
+        Food food6 = new Food();
+        addObject(food6,92,210);
+        AntHill antHill2 = new AntHill();
+        addObject(antHill2,531,530);
+        removeObject(antHill2);
+        AntHill antHill3 = new AntHill(1000);
+        addObject(antHill3,513,468);
     }
 }
